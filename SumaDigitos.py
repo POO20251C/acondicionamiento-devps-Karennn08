7d216d2109d2c21756c284c8325a187b13c4f3c9
karen <karenarangov@gmail.com>
2025-02-05 08:07:36 -0500

    prueba de push

---
+++ SumaDigitos.py
@@ -0,0 +1,10 @@
+## Ejercicio 1: Suma de Dígitos
+##Descripción: Dado un número entero, calcula la suma de todos sus dígitos.
+##Entrada:Una línea que contenga un número entero.
+##Salida:Un entero que es la suma de los dígitos.
+
+def main():
+    numero = int(input("Numero al que desea sumarle los digitos: "))
+    
+main()
+    
